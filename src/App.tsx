4e615b40e78c3e0fe2378935612e54cf0b3e5b70
hujianbo <466636709@qq.com>
2020-10-21 22:40:09 +0800

    add button style part1

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,21 @@ import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 function App() {
   return (
     <div className="App">
-      <Button disabled>df</Button>
-      <Button>df</Button>
+      <Button>Hello</Button>
+      <Button disabled> Disabled Button</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Large Primary
+      </Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+        Small Danger
+      </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Large Primary
+      </Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com">
+        {" "}
+        Baidu Link{" "}
+      </Button>
     </div>
   );
 }
