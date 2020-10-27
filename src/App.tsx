03df4f7739870ebc4ebd7850c5b066a10d61048a
hujianbo <466636709@qq.com>
2020-10-27 21:14:45 +0800

    add context to menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 import Alert, { AlertType } from "./components/Alert/alert";
-import Menu from "./components/Menu/Menu";
+import Menu from "./components/Menu/menu";
 import MenuItem from "./components/Menu/menuItem";
 function App() {
   return (
     <div className="App">
-      <Menu>
-        <MenuItem>MenuItem</MenuItem>
-        <MenuItem>MenuItem</MenuItem>
-        <MenuItem>MenuItem</MenuItem>
+      <Menu
+        onSelect={(index) => {
+          alert(index);
+        }}
+      >
+        <MenuItem index={1}>MenuItem</MenuItem>
+        <MenuItem index={2}>MenuItem</MenuItem>
+        <MenuItem index={3} disabled>
+          MenuItem
+        </MenuItem>
       </Menu>
     </div>
   );
