98089c30c063c9e949adbdbdf76cd990e82cf94e
hujianbo <466636709@qq.com>
2020-10-27 16:19:01 +0800

    add menuItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,16 @@
 import React from "react";
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 import Alert, { AlertType } from "./components/Alert/alert";
-
+import Menu from "./components/Menu/Menu";
+import MenuItem from "./components/Menu/menuItem";
 function App() {
   return (
     <div className="App">
-      <Alert
-        title="alert"
-        description="Success"
-        type={AlertType.Success}
-        closable
-        onClose={() => {
-          // alert("close");
-        }}
-      />
-      <Alert
-        title="alert"
-        description="Danger"
-        type={AlertType.Danger}
-        closable
-        onClose={() => {
-          // alert("close");
-        }}
-      />
-      <Alert
-        title="alert"
-        description="Default"
-        type={AlertType.Default}
-        closable
-        onClose={() => {
-          // alert("close");
-        }}
-      />
-      <Alert
-        title="alert"
-        description="Warning"
-        type={AlertType.Warning}
-        closable
-        onClose={() => {
-          // alert("close");
-        }}
-      />
+      <Menu>
+        <MenuItem>MenuItem</MenuItem>
+        <MenuItem>MenuItem</MenuItem>
+        <MenuItem>MenuItem</MenuItem>
+      </Menu>
     </div>
   );
 }
